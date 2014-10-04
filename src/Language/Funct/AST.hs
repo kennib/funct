860f550ed8f7f636834627450ea3e3fefd88bc6b
Kenni Bawden <kenni.bawden@gmail.com>
2014-10-04 23:57:23 +1000

    Created an AST for the language

---
+++ src/Language/Funct/AST.hs
@@ -0,0 +1,22 @@
+module Language.Funct.AST where
+
+import Data.TypeHash
+
+data Program = Program [Definition] 
+    deriving (Show)
+
+data Definition = AliasDefinition Alias Hash | TypeDefinition Type | FunctionDefinition Function
+    deriving (Show)
+
+data Alias = Alias String
+    deriving (Show)
+data Hash = Hash TypeHash
+    deriving (Show)
+
+data Type = SumType [Type] | ProductType [Type] | TypeHash Hash | TypeAlias Alias
+    deriving (Show)
+
+data Function = Function [Type] [Value String]
+    deriving (Show)
+data Value a = Value a
+    deriving (Show)
